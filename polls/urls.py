6dff4aa31eb538941f8c46b9cd9bff5be4d05fe8
gabrielTMH <74377120+gabrielTMH@users.noreply.github.com>
2022-03-01 14:14:00 -0800

    added some basic views and connected Questions to the admin site

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+# ex: /polls/
+    path('<int:question_id>/', views.detail, name='detail'),
+# ex: /polls/
+    path('<int:question_id>/results', views.results, name='iresults'),
+# ex: /polls/
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
+    latest_question_list= Question.onjects.order_by()
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("you're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("you're looking at the results of question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s" % question_id)
