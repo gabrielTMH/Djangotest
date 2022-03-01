c7aad7a80423a134a18ea1c696d779de9d2d8f5d
gabrielTMH <74377120+gabrielTMH@users.noreply.github.com>
2022-03-01 15:04:25 -0800

    added some templates and made the views a little more 'real'

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
+from django.http import Http404
 
 def index(request):
-    latest_question_list= Question.onjects.order_by()
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context ={
+        'latest_question_list': latest_question_list,
+    }
+    #output=",".join([q.question_text for q in latest_question_list])
+    #return HttpResponse(template.render(context,request))
+    return render(request, 'polls/index.html',context)
 
 
+#giving myself a little 404 error if the user trys to request a question id that does not exist
 def detail(request, question_id):
-    return HttpResponse("you're looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("whoopsie that doewsnt exist >_<")
+    return render(request, 'polls/detail.html', {'question':question})
+    #return HttpResponse("you're looking at question %s" % question_id)
 
 
 def results(request, question_id):
